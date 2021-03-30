121579eb2ae462b56d100244e87064832d20c6f5
Wenhao Ge <ge.wen@northeastern.edu>
2021-03-29 21:20:11 -0700

    created list widget

---
+++ src/components/widgets/list-widget.js
@@ -0,0 +1,50 @@
+import React from 'react'
+
+const ListWidget = ({widget, setWidget, editing}) => {
+
+    return (
+        <div>
+            <h2>List Widget</h2>
+
+            {
+                !editing &&
+                    <>
+                        {
+                            widget.ordered &&
+                            <ol>
+                                {
+                                    widget.text.split("\n").map(item => {
+                                        return (<li>{item}</li>)
+                                    })
+                                }
+                            </ol>
+                        }
+                        {
+                            !widget.ordered &&
+                            <ul>
+                                {
+                                    widget.text.split("\n").map(item => {
+                                        return(<li>{item}</li>)
+                                    })
+                                }
+                            </ul>
+                        }
+                    </>
+            }
+            {
+                editing &&
+                <div>
+                    <input type="checkbox"/>
+                    Ordered
+                    <br/>
+                    List Items
+                    <textarea rows={10} value={widget.text} className="form-control">
+
+                    </textarea>
+                </div>
+            }
+        </div>
+    )
+}
+
+export default ListWidget
